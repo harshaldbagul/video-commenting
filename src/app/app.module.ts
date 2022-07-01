78ecf0e1afac9e32c7610e99721f48af34ebd9a4
Harshal Bagul <HarshalB@talentica-all.com>
2022-07-01 16:45:31 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommentDialogComponent } from './dialogs/comment-dialog/comment-dialog.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { FormatTimePipe } from './pipes/format-time.pipe';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CommentDialogComponent, FormatTimePipe],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,MatFormFieldModule,MatInputModule
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pipes/format-time.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatTime',
+})
+export class FormatTimePipe implements PipeTransform {
+  transform(timeInSeconds: number): string {
+    if (!timeInSeconds) return '';
+
+    const h = Math.floor(timeInSeconds / 3600);
+    const m = Math.floor((timeInSeconds % 3600) / 60);
+    const s = Math.floor((timeInSeconds % 3600) % 60);
+
+    const hDisplay = h > 0 ? (h < 10 ? '0' + h : h) + ' : ' : '';
+    const mDisplay = (m > 0 ? (m < 10 ? '0' + m : m) : '00') + ' : ';
+    const sDisplay = s > 0 ? (s < 10 ? '0' + s : s) : '00';
+
+    return hDisplay + mDisplay + sDisplay;
+  }
+}
